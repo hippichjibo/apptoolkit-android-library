628714c447ce692c54dd69251bf244c6ba162013
Susan Pedicini <susan.pedicini@jibo.com>
2018-04-03 11:15:11 -0400

    prod not  stg

--- app/src/main/java/com/jibo/apptoolkit/android/example/CommandSampleApplication.kt
+++ app/src/main/java/com/jibo/apptoolkit/android/example/CommandSampleApplication.kt
@@ -2,7 +2,6 @@ package com.jibo.apptoolkit.android.example
 
 import android.app.Application
 import com.jibo.apptoolkit.android.JiboRemoteControl
-import com.jibo.apptoolkit.protocol.utils.Commons
 
 /**
  * Created by calvinator on 3/29/18.
@@ -11,12 +10,6 @@ class CommandSampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        try {
-            Commons.setRootEndpoint(Commons.STG_ENDPOINT)
-        } catch (e: Commons.InvalidParameterValueException) {
-            e.printStackTrace()
-        }
 
         JiboRemoteControl.init(this, getString(R.string.appId), getString(R.string.appSecret))
 
